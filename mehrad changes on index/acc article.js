af76db6cfe1d246140a4948828391f15e088b3af
mehrad <mehrad.kavian@gmail.com>
2012-07-02 14:59:34 +0430

    a lot of changes and begin working on each product

---
+++ mehrad changes on index/acc article.js
@@ -0,0 +1,15 @@
+
+var hSelector= document.getElementsByClassName('acc-container').item(0).getElementsByClassName('selector');
+var accArticles = document.getElementsByClassName('acc-container').item(0).getElementsByTagName('ul');
+hSelector.onClick = {accArticles.style.height='100px';}
+	
+
+
+
+
+
+
+
+
+
+
