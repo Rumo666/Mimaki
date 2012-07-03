9029aa63e0269c53ada53e0b454488b44074985c
mehrad <mehrad.kavian@gmail.com>
2012-07-03 22:29:08 +0430

    no idea

--- mehrad changes on index/acc-article.js
+++ mehrad changes on index/acc-article.js
@@ -1,8 +1,18 @@
+var maximise,target,nextslide;
+
+
 maximise = document.getElementById('ass');
 target =   document.getElementById('ss');
 
+nextslide = function (){
+
+	target.style.height='200px;'
+
+};
+
+
 maximise.onclick=function (){
-	target.style.height ='200px';
+	nextslide();
 };
 
 
