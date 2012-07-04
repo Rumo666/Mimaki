446ea3130b860f75f7b1279570fe659e3c3e1611
mehrad <mehrad.kavian@gmail.com>
2012-07-04 20:37:06 +0430

    now you can even open and close it ;)

--- mehrad changes on index/acc-article.js
+++ mehrad changes on index/acc-article.js
@@ -3,8 +3,12 @@ var maximise,target,nextSlide;
 
 
 nextSlide = function (){
+	var maximise =document.getElementsByClassName('acc-container').item().getElementsByClassName('acdc').item();
 
- var maximise =document.getElementsByClassName('acc-container').item().getElementsByClassName('acdc').item().style.height='200px';
+	if(maximise.style.height<='10px')
+		maximise.style.height='200px';
+	else
+		maximise.style.height='10px';
 
 };
 
