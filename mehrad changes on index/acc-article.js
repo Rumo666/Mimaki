3f3ef7a836b9bc3a3178238d577f0bcca57f8307
mehrad <mehrad.kavian@gmail.com>
2012-07-04 21:43:48 +0430

    some changes

--- mehrad changes on index/acc-article.js
+++ mehrad changes on index/acc-article.js
@@ -3,7 +3,7 @@ var maximise,target,nextSlide;
 
 
 nextSlide = function (){
-	var maximise =document.getElementsByClassName('acc-container').item().getElementsByClassName('acdc').item(012);
+	var maximise =document.getElementsByClassName('acc-container').item().getElementsByClassName('acdc').items();
 
 	if(maximise.style.height<='10px')
 		maximise.style.height='200px';
