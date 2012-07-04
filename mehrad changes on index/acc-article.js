243ee4c1458834eca6d048978f812f117756f450
mehrad <mehrad.kavian@gmail.com>
2012-07-05 00:07:46 +0430

    after a hard time work i found the way...

--- mehrad changes on index/acc-article.js
+++ mehrad changes on index/acc-article.js
@@ -3,12 +3,13 @@ var maximise,target,nextSlide;
 
 
 nextSlide = function (){
-	var maximise =document.getElementsByClassName('acc-container').item().getElementsByClassName('acdc').items();
-
+for(var i=0;i<3;i++){
+	var maximise =document.getElementsByClassName('acc-container').item().getElementsByTagName('ul').item(i);
 	if(maximise.style.height<='10px')
 		maximise.style.height='200px';
 	else
 		maximise.style.height='10px';
+}
 
 };
 
