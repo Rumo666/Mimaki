fa70d8ba38310ade6374fc86ea88ac7a931527e0
mehrad <mehrad.kavian@gmail.com>
2012-07-04 20:07:02 +0430

    some changes

--- mehrad changes on index/acc-article.js
+++ mehrad changes on index/acc-article.js
@@ -4,7 +4,7 @@ var maximise,target,nextSlide;
 
 nextSlide = function (){
 
- document.getElementsByClassName('accmain-container').item(0).getElementsByClassName('acc-container').item(0).getElementsByTagName('ul').style.height='200px';
+ getElementsByClassName('acc-container').item(0).getElementsByClassName("acdc").style.height='200px';
 
 };
 
