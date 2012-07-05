494d7063c5964caf81ddfa0dd6a53ed70e036cbd
mehrad <mehrad.kavian@gmail.com>
2012-07-05 18:12:13 +0430

    trting to make in diferent

--- mehrad changes on index/acc-article.js
+++ mehrad changes on index/acc-article.js
@@ -1,10 +1,11 @@
 var maximise,target,nextSlide;
+var target =document.getElementsByClassName('acc-container').item().getElementsByTagName('h3').item();
 
 
 
-nextSlide = function (){
+	target.onclick =function (){
 for(var i=0;i<3;i++){
-	var maximise =document.getElementsByClassName('acc-container').item().getElementsByTagName('ul').item(i);
+	 maximise =document.getElementsByClassName('acc-container').item().getElementsByTagName('ul').item(i);
 	if(maximise.style.height<='10px')
 		maximise.style.height='200px';
 	else
